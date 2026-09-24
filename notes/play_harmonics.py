1cf843dd473abfb3ef52cc03a98167b49571f789
JLS <jonathan.sievers@gmail.com>
2026-09-24 01:37:24 -0400

    added harmonics note

--- notes/play_harmonics.py
+++ notes/play_harmonics.py
@@ -1,18 +1,26 @@
 import numpy as np
 import sounddevice as sd
 
-tmax=10 #10 seconds of playing
+tmax=20 #how many seconds of playing
 nu0=220 #fundamental will be this frequency
 fs=44100 #standard audio sample rate
 
 t=np.arange(tmax*fs)/fs
 
-amps=[1, 1, 1, 1., 0.5] #amplitude of harmonics
+amps=[1, 0, 1, 0, 1 ,0, 0.5] #amplitude of harmonics.  These are the numbers you should play with
+#amps=[1]
 
+#sum all the harmonics together, with the amplitudes we set.
 tot=0
 for i in range(len(amps)):
     k=i+1
     tot=tot+np.sin(t*nu0*2*np.pi*k)*amps[i]
 
 tot=tot/tot.max()
-sd.play(tot,fs)
+
+#convert our mono signal into a stereo one
+to_play=np.zeros([len(tot),2])
+to_play[:,0]=tot
+to_play[:,1]=tot
+#play what we made!
+sd.play(to_play,fs)
